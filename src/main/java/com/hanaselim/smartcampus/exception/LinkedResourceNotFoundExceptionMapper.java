0b0614c97b70c50351185630769d7e33edc7d722
Hana Selim <hana.nrhny@gmail.com>
2026-05-03 00:45:27 +0100

    Implemented sensor resource and linked resource not found exception & mapper

---
+++ src/main/java/com/hanaselim/smartcampus/exception/LinkedResourceNotFoundExceptionMapper.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.hanaselim.smartcampus.exception;
+
+import com.hanaselim.smartcampus.model.ErrorMessage;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+/**
+ *
+ * @author Dell
+ */
+@Provider
+public class LinkedResourceNotFoundExceptionMapper implements ExceptionMapper<LinkedResourceNotFoundException>{
+    @Override
+    public Response toResponse(LinkedResourceNotFoundException ex){
+        ErrorMessage error = new ErrorMessage(
+                ex.getMessage(),
+                422,
+                "Linked resource not found"
+        );
+        
+        return Response.status(422).entity(error).type(javax.ws.rs.core.MediaType.APPLICATION_JSON).build();
+    }
+}
---
+++ src/main/java/com/hanaselim/smartcampus/resource/SensorResource.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.hanaselim.smartcampus.resource;
+
+import com.hanaselim.smartcampus.database.DataStore;
+import com.hanaselim.smartcampus.exception.LinkedResourceNotFoundException;
+import com.hanaselim.smartcampus.model.Sensor;
+import java.util.Collection;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author Dell
+ */
+@Path("/sensors")
+public class SensorResource {
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response addSensor(Sensor sensor) {
+        
+        if (sensor == null || sensor.getRoomId() == null) {
+            throw new LinkedResourceNotFoundException("Room ID is missing in request");
+        }
+        if (!DataStore.rooms.containsKey(sensor.getRoomId())) {
+            throw new LinkedResourceNotFoundException(
+                    "Room with ID " + sensor.getRoomId() + " does not exist"
+            );
+        }
+
+        DataStore.sensors.put(sensor.getId(), sensor);
+        DataStore.rooms.get(sensor.getRoomId()).getSensorIds().add(sensor.getId());
+
+        return Response.status(Response.Status.CREATED).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Collection<Sensor> getSensors(@QueryParam("type") String type) {
+
+        if (type == null) {
+            return DataStore.sensors.values();
+        }
+
+        return DataStore.sensors.values().stream().filter(s -> s.getType().equalsIgnoreCase(type)).toList();
+    }
+}
