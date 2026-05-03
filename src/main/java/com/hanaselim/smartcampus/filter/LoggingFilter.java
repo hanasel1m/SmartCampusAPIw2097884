4b60fc1852767b2f8bce23f60fb61a1ffbe5c66c
Hana Selim <hana.nrhny@gmail.com>
2026-05-03 02:20:37 +0100

    Created logging filter

---
+++ src/main/java/com/hanaselim/smartcampus/filter/LoggingFilter.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.hanaselim.smartcampus.filter;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+
+
+/**
+ *
+ * @author Dell
+ */
+@Provider
+public class LoggingFilter implements ContainerRequestFilter, ContainerResponseFilter{
+    
+    private static final Logger LOGGER = Logger.getLogger(LoggingFilter.class.getName());
+    
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException{
+        LOGGER.info("Request: "+requestContext.getMethod()+" "+requestContext.getUriInfo().getRequestUri());
+        
+    }
+    
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException{
+        LOGGER.info("Response Status: "+ responseContext.getStatus());
+    }
+    
+}
