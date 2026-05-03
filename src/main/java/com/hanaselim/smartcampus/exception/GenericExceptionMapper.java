c432b4eac41716627a3b8b6e1c62ee243257c68f
Hana Selim <hana.nrhny@gmail.com>
2026-05-03 02:10:07 +0100

    Created generic exception mapper

---
+++ src/main/java/com/hanaselim/smartcampus/exception/GenericExceptionMapper.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.hanaselim.smartcampus.exception;
+
+import com.hanaselim.smartcampus.model.ErrorMessage;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+/**
+ *
+ * @author Dell
+ */
+@Provider
+public class GenericExceptionMapper implements ExceptionMapper<Throwable>{
+    @Override
+    public Response toResponse(Throwable ex){
+        ErrorMessage error = new ErrorMessage(
+                "Internal server error",
+                500,
+                "Unexpected error occured"
+        );
+        
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(error).type(MediaType.APPLICATION_JSON).build();
+    }
+}
